6168d10810e7ac6641b5ebb836682f0be98a2e02
Fyreum <luka.vogel@gmx.de>
2025-02-12 10:11:58 +0100

    Doesnt work either. Postponing...

--- src/lib/components/ProgrammingLanguagesSection.svelte
+++ src/lib/components/ProgrammingLanguagesSection.svelte
@@ -107,7 +107,7 @@
         arrowBtns.forEach(btn => {
             btn.addEventListener("click", () => {
                 carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-                setCurrentLanguage(mod(btn.id == "left" ? currentLanguage - 1 : currentLanguage + 1, languages.length), false);
+                setCurrentLanguage(mod(btn.id == "left" ? currentLanguage - 1 : currentLanguage + 1, languages.length));
             });
         });
 
